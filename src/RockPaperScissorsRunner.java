fe4e9443e01c5303f4a8311f5649ce59883af591
derossca <derossca@mail.uc.edu>
2024-10-01 07:32:52 -0400

    made RPS runner

---
+++ src/RockPaperScissorsRunner.java
@@ -0,0 +1,10 @@
+import javax.swing.*;
+
+public class RockPaperScissorsRunner {
+
+    public static void main(String[] args) {
+        JFrame frame = new RockPaperScissorsFrame();
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setVisible(true);
+    }
+}
